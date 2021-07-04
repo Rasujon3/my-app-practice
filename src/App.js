a2cc5625b6b46abeab7406710cdec1290fd44979
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-07-05 01:29:30 +0600

    1. Creating Component List - React for Frontend Development with Redux - Simanta Paul
    1. Creating Component List - React for Frontend Development with Redux - Simanta Paul

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Book from './components/Book';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+class App extends Component {
+  state = {
+    books: [
+      { bookName: "1984", writer: "George Orwell" },
+      { bookName: "The Da Vonci Code", writer: "Dan Brown" },
+      { bookName: "The alchemist", writer: "Paulo Coelho" }
+    ],
+    otherPropos: "I am some other Props",
+  };
+
+  changeBookState = (newBookName) => {
+    this.setState({
+      books: [
+        { bookName: newBookName, writer: "George Orwell" },
+        { bookName: "The Da Vonci Code", writer: "Dan Brown" },
+        { bookName: "Metmorphosis", writer: "Franz Kafka" }
+      ],
+    });
+  }
+
+  changeWithInputChange = (event) => {
+    this.setState({
+      books: [
+        { bookName: event.target.value, writer: "George Orwell" },
+        { bookName: "The Da Vonci Code", writer: "Dan Brown" },
+        { bookName: "Metmorphosis", writer: "Franz Kafka" }
+      ],
+    });
+  }
+
+  render() {
+    const style = {
+      border: "1px solidd red",
+      borderRadius: "5px",
+      backgroundColor: "black",
+      color: "white"
+    };
+
+    // const bookState = this.state.books;
+
+    const books = this.state.books.map((book) => {
+      return (
+        <Book
+          bookName={book.bookName}
+          bookName={book.writer}
+        />
+      );
+    })
+
+    // console.log(bookState);
+    console.log(books);
+
+    return (
+      <div className="App">
+
+        <h1 style={style}>Book List</h1>
+        <button onClick={() => this.changeBookState("Nineteen Eithty-Four")}>Change State</button>
+        <input type="text" onChange={this.changeWithInputChange} />
+        {books}
+
+      </div>
+    );
+
+  }
 }
 
 export default App;
